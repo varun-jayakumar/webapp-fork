d1020cb6513f1af03ce4ee39ab0447333b3fd052
Varun Jayakumar <jayakumar.va@northeastern.edu>
2024-02-13 17:37:43 -0500

    update: sync(force: true) for test env

--- src/models/index.js
+++ src/models/index.js
@@ -3,9 +3,13 @@ import { sequelize } from "../config/database.js";
 
 export let User;
 
-const initializeModels = () => {
+export const initializeModels = () => {
   User = sequelize.define("User", user_model);
-  User.sync({ alter: true });
+  if (process.env.NODE_ENV === "test") {
+    User.sync({ force: true });
+  } else {
+    User.sync({ alter: true });
+  }
 };
 
 const validateInitializtion = () => {
