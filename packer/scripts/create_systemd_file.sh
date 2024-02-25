e40ef8861bb97971dec89f371824f5867afa2466
Varun Jayakumar <jayakumar.va@northeastern.edu>
2024-02-25 13:19:35 -0500

    edited systemd file

--- packer/scripts/create_systemd_file.sh
+++ packer/scripts/create_systemd_file.sh
@@ -3,7 +3,6 @@ cd /lib/systemd/system || exit 1
 sudo bash -c 'cat << 'EOF' > csye6225.service
 [Unit]
 Description=CSYE 6225 App
-ConditionPathExists=/opt/webapp/.env
 After=network.target
 
 [Service]
@@ -11,6 +10,7 @@ Type=simple
 User=csye6225
 Group=csye6225
 WorkingDirectory=/opt/webapp/
+ExecStartPre=/bin/bash -c 'while true; do if [ -f /opt/webapp/.env ]; then echo "File exists. Exiting."; exit 0; else echo "File does not exist. Retrying in 1 second..."; sleep 1; fi; done'
 ExecStart=/usr/bin/node -r dotenv/config /opt/webapp/server.js
 Restart=always
 RestartSec=3
