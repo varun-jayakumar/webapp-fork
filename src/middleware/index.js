9e197ffa6a788d02ab2e8b9b29e6d15d3efbc832
Varun Jayakumar <jayakumar.va@northeastern.edu>
2024-03-20 12:29:51 -0400

    added request logger

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,11 +1,14 @@
 import { validatePayload } from "./validate-payload.js";
 import authenticaiton from "./authentication.js";
 import dbConnectionStatus from "./db-connection-status.js";
+import requestLogger from "./request_logger.js";
+
 const initializeMiddlewares = (app) => {
   validatePayload(app);
 };
 
 const middlewares = {
+  requestLogger: requestLogger,
   authenticaiton: authenticaiton,
   dbConnectionStatus: dbConnectionStatus,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
+import { middlewares } from "../middleware/index.js";
 import mainRouter from "./main-router.js";
 import userRouter from "./user-router.js";
 export default (app) => {
   app.use("/", mainRouter);
   app.use("/v1/user", userRouter);
-  app.all("*", (req, res) => {
+  app.all("*", middlewares.requestLogger, (req, res) => {
     res.status(404);
     res.set("cache-control", "no-cache").end();
   });
