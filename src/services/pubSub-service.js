776d96b5495eb440ea93782060bbba74f2a9d43f
Varun Jayakumar <jayakumar.va@northeastern.edu>
2024-03-25 00:45:49 -0400

    fixed tests

--- src/services/pubSub-service.js
+++ src/services/pubSub-service.js
@@ -6,13 +6,15 @@ const topicName = process.env.PUB_TOPIC;
 const pubSubClient = new PubSub({ projectId });
 
 export async function publishMessage(data) {
-  const dataBuffer = Buffer.from(data);
-  try {
-    const messageId = await pubSubClient
-      .topic(topicName)
-      .publishMessage({ data: dataBuffer });
-    logger.debug(`Message ${messageId} published.`);
-  } catch (error) {
-    logger.debug(`Received error while publishing: ${error.message}`);
+  if (process.env.NODE_ENV !== "test") {
+    const dataBuffer = Buffer.from(data);
+    try {
+      const messageId = await pubSubClient
+        .topic(topicName)
+        .publishMessage({ data: dataBuffer });
+      logger.debug(`Message ${messageId} published.`);
+    } catch (error) {
+      logger.debug(`Received error while publishing: ${error.message}`);
+    }
   }
 }
